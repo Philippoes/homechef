b5d260d97898d83bc1d092a2a63793635a9aa525
rmzse <rodrigo@therawdelicious.com>
2017-01-11 17:29:57 +0100

    test for add dish button with id for the dish on display passing

--- features/step_definitions/add_dish_to_order_steps.rb
+++ features/step_definitions/add_dish_to_order_steps.rb
@@ -1,6 +1,6 @@
 When(/^I click the "([^"]*)" button for "([^"]*)"$/) do |button, dish|
 	id = Dish.find_by(name: dish).id
-	within(:css, "li#dish_#{id}") do
+	within(:css, "#dish_#{id}") do
 		click_button button
 	end
 end
