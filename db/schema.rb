41854d93d1f95e29b60a2d1a298c4842ce388e8b
rmzse <rodrigo@therawdelicious.com>
2017-01-11 21:33:24 +0100

    add orderitem model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111201750) do
+ActiveRecord::Schema.define(version: 20170111203201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20170111201750) do
     t.integer  "price"
     t.datetime "ready_time"
     t.integer  "portions"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "orderitems", force: :cascade do |t|
+    t.datetime "pickup_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
