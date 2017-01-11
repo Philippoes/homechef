4a541ae63b1786a75b29bb90d15859a7aa82c737
rmzse <rodrigo@therawdelicious.com>
2017-01-11 21:19:49 +0100

    add buyingorder (list) model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110203947) do
+ActiveRecord::Schema.define(version: 20170111201750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buyingorders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
