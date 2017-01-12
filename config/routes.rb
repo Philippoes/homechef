1d157bd005285606d5127aaab6aa6feb33d5ac97
rmzse <rodrigo@therawdelicious.com>
2017-01-12 10:38:03 +0100

    add orderitems controller and edit it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'orderitems/resources'
-
-  get 'buyingorderlists/create'
 
   root controller: :landing, action: :index
   resources :dishes, only: [:index, :show]
-  resources :buyingorderlists, :sellingorderlists, only: [:index, :create, :show, :edit, :update, :destroy ]
+  resources :buyingorderlists, :sellingorderlists, :orderitems only: [:index, :create, :show, :edit, :update, :destroy ]
 end
