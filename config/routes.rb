35c0b1db132638fe0de74f49a71c202f50aaaa3f
rmzse <rodrigo@therawdelicious.com>
2017-01-12 10:39:59 +0100

    fixes orderitems routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root controller: :landing, action: :index
   resources :dishes, only: [:index, :show]
-  resources :buyingorderlists, :sellingorderlists, :orderitems only: [:index, :create, :show, :edit, :update, :destroy ]
+  resources :buyingorderlists, :sellingorderlists, :orderitems, only: [:index, :create, :show, :edit, :update, :destroy ]
 end
