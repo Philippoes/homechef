1f341968cb656066bbd76ad0b9ac23e8a3e32fa4
rmzse <rodrigo@therawdelicious.com>
2017-01-12 14:59:52 +0100

    remove selling order controller

--- app/controllers/selling_orders_controller.rb
+++
@@ -1,9 +0,0 @@
-class SellingOrdersController < ApplicationController
-	def create
-		if @sellingorder != nil
-			@sellingorder = Sellingorder.find_by(session: current_session)
-		else
-			@sellingorder = Sellingorder.create()
-		end
-	end
-end
