2d589c68ab1719be9cc9a30113c3d98d441204e3
rmzse <rodrigo@therawdelicious.com>
2017-01-12 15:00:19 +0100

    remove order items controller

--- app/controllers/order_items_controller.rb
+++
@@ -1,7 +0,0 @@
-class OrderItemsController < ApplicationController
-	def create
-		Orderitem.create(pickup_time: Dishes.find(params[:id]).readytime, dish: Dishes.find(params[:id]), buyingorder: @buyingorder, sellingorder: @sellingorder)
-		redirect_to root_path
-		flash[:notice] = "Successfully added to order"
-	end
-end
