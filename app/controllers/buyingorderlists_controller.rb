205e25260d822e1fed972bb28d18cca6ec4d1f34
rmzse <rodrigo@therawdelicious.com>
2017-01-12 13:30:24 +0100

    add functionality to the button on the view to pass the dish to the controller!

--- app/controllers/buyingorderlists_controller.rb
+++ app/controllers/buyingorderlists_controller.rb
@@ -1,9 +1,9 @@
 class BuyingorderlistsController < ApplicationController
   def create
     if @buyingorder != nil
-      @buyingorder = Buyingorder.find_by(session: current_session)
+      @buyingorder = Buyingorder.find_by(user: current_user)
     else
-      @buyingorder = Buyingorder.create()
+      @buyingorder = Buyingorder.create
     end
   end
 end
