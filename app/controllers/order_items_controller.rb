46f2a947cab5ec57f215bf5a560be99101eede18
rmzse <rodrigo@therawdelicious.com>
2017-01-12 14:53:04 +0100

    create new order_items_controller and remove old orderitems controller dependencies remaining from file delete

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,6 @@
 class OrderItemsController < ApplicationController
 	def create
-		Orderitem.create(pickup_time: Dishes.find(params[id]).readytime, dish: Dishes.find(params[id]), buyingorder: @buyingorder, sellingorder: @sellingorder)
+		Orderitem.create(pickup_time: Dishes.find(params[:id]).readytime, dish: Dishes.find(params[:id]), buyingorder: @buyingorder, sellingorder: @sellingorder)
 		redirect_to root_path
 		flash[:notice] = "Successfully added to order"
 	end
