02ea14f776a31d9cc15fe8b2a24953526585c509
rmzse <rodrigo@therawdelicious.com>
2017-01-12 14:41:19 +0100

    re-generate the controllers for order items, buying order and selling order

--- app/controllers/sellingorder_controller.rb
+++
@@ -1,9 +0,0 @@
-class SellingorderController < ApplicationController
-  def create
-    if @sellingorder != nil
-      @sellingorder = Sellingorder.find_by(session: current_session)
-    else
-      @sellingorder = Sellingorder.create()
-    end
-  end
-end
