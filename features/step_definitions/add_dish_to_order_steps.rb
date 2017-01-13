145e334fde44249bcab8e9521b42806f53e1fc4b
Philip <Philip.zdb@gmail.com>
2017-01-13 12:21:24 +0100

    Adds extra steps in acceptance test for payment in order to intergrate actual order

--- features/step_definitions/add_dish_to_order_steps.rb
+++ features/step_definitions/add_dish_to_order_steps.rb
@@ -5,3 +5,13 @@ When(/^I click the "([^"]*)" button for "([^"]*)"$/) do |button, dish|
     click_link_or_button button
   end
 end
+
+
+And(/^there are two dishes in my order$/) do
+  %q{
+    When I am on the "landing" page
+    And I click the "Add dish" button for "taco"
+    And I click the "Add dish" button for "meatballs"
+    And there should be "2" items on the last order
+  }
+end
