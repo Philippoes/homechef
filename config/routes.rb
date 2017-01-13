1c976aeff1ed537775a0359531ba2588f71d955c
Philip <Philip.zdb@gmail.com>
2017-01-13 12:01:28 +0100

    Adds charges route for stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root controller: :landing, action: :index
   resources :dishes, only: [:index, :show]
+  resources :charges, only: [:create]
   get :checkout, controller: :landing, action: :checkout
   post :add_to_order, controller: :orders, action: :add_to_order
 end
