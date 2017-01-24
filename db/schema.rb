1add3fea2e8592cac72a6f5252a09f8d4effc696
Philip <Philip.zdb@gmail.com>
2017-01-24 13:21:43 +0100

    Adds bought column to order, and sets default value to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124121514) do
+ActiveRecord::Schema.define(version: 20170124122059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20170124121514) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "bought",     default: false
   end
 
 end
