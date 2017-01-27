ae24375b9f0334542570dde56d7cb1f625d81c24
Philip <Philip.zdb@gmail.com>
2017-01-27 11:19:52 +0100

    Adds warden test helpers + Fixes add dish to order feature to work with users

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def add_to_order
-    @order = Order.find_by(finalized: false) || Order.create
+    @order = Order.find_by(user: current_user, finalized: false) || Order.create(user: current_user)
     dish = Dish.find(params[:dish_id])
     @order.add(dish, dish.price)
     flash[:notice] = "Successfully added to order"
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,3 +1,9 @@
 Then(/^show me the page$/) do
   save_and_open_page
 end
+
+And(/^that there is a user logged in with an email of "([^"]*)"$/) do |email|
+    FactoryGirl.create(:user, email: email)
+    user = User.find_by(email: email)
+    login_as(user, scope: :user)
+end
