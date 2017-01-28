e125c830c2e72d4cd5cda94575fefbe811f3e425
Philip <Philip.zdb@gmail.com>
2017-01-28 17:40:07 +0100

    Replaces old syntax from migration

--- db/migrate/20170126113849_add_finalized_to_orders.rb
+++ db/migrate/20170126113849_add_finalized_to_orders.rb
@@ -1,5 +1,5 @@
 class AddFinalizedToOrders < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :finalized, :boolean, :default => false
+    add_column :orders, :finalized, :boolean, default: false
   end
 end
