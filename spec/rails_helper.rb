e9260671c3d4206f3ded1524d7dfd9b039c1dc8a
Philip <Philip.zdb@gmail.com>
2017-02-06 17:26:35 +0100

    Adds additional setup to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,4 +22,14 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
