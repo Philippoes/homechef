636b261f73a92470bbb5d64aea03018750591b16
rmzse <rodrigo@therawdelicious.com>
2017-02-07 21:29:00 +0100

    add sad path to order spec

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::OrdersController < ApiController
   def create
     create_order
     dish = Dish.find(params[:dish_id])
-    @order.add(dish, dish.price)
-    render json: {message: "Successfully added Pizza"}
+    if dish.id/dish.id == 1
+      @order.add(dish, dish.price)
+      render json: {message: "Successfully added Pizza"}
+    else
+      render json: {message: "Something went wrong, you did not add a dish to your order"}
+    end
   end
 
   private
