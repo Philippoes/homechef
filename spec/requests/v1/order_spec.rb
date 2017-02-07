fce75dfc0cc928687a3367bd19e4fcfc17b4a50f
rmzse <rodrigo@therawdelicious.com>
2017-02-07 21:23:36 +0100

    add sad path to order spec

--- spec/requests/v1/order_spec.rb
+++ spec/requests/v1/order_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe Api::V1::OrdersController, type: :request do
       expect(response_json['message']).to eq "Successfully added Pizza"
       expect(response.status).to eq 200
     end
+
+    it 'should give error if dish_id is invalid' do
+      dish = FactoryGirl.create(:dish)
+      post '/api/v1/orders', params: {
+          dish_id: dish.name }
+      expect(response_json['message']).to eq "Something went wrong, you did not add a dish to your order"
+      expect(response.status).to eq 422
+    end
   end
 end
