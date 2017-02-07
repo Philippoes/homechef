4fefcebdc96169814efa493d4ac8510f937ecd0f
rmzse <rodrigo@therawdelicious.com>
2017-02-07 20:27:17 +0100

    add success message and pass

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::OrdersController < ApiController
   def create
+    create_order
+    dish = Dish.find(params[:dish_id])
+    @order.add(dish, dish.price)
     render json: {message: "Successfully added Pizza"}
   end
+
+  private
+
+  def create_order
+    @order = Order.create(user: current_user)
+  end
+
 end
